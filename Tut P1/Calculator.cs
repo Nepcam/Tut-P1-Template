a2ea10046e258b9fc077cfad87125f46a88b6ba3
Nepcam <cameron.nepe@gmail.com>
2020-08-12 14:33:57 +1200

    Calculator class

---
+++ Tut P1/Calculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tut_P1
+{
+    class Calculator
+    {
+        private int _value;
+
+        /// <summary>
+        /// Initialises the object to it's initial value
+        /// </summary>
+        /// <param name="intialValue"></param>
+        public Calculator(int intialValue)
+        {
+            _value = intialValue;
+        }
+
+    }
+}
--- Tut P1/Form1.cs
+++ Tut P1/Form1.cs
@@ -42,14 +42,25 @@ namespace Tut_P1
         private void buttonStudent_Click(object sender, EventArgs e)
         {
             Pen pen1 = new Pen(Color.Blue, 2);
-            Student s1 = new Student();
+            Student s1 = new Student(103, "Brown", "Smoke");
             Student s2 = new Student(101, "Doe", "John");
             Student s3 = new Student(102, "Brown", "Mark");
+            Student s4 = null;
 
             //listBoxData.Items.Add(s2.Id + " " + s2.Firstname + " " + s2.Surname + " " + s2.Haspaid);
-            listBoxData.Items.Add(s1.ToString());
-            listBoxData.Items.Add(s2.ToString());
-            listBoxData.Items.Add(s3.ToString());
+            //listBoxData.Items.Add(s1.ToString());
+            //listBoxData.Items.Add(s2.ToString());
+            //listBoxData.Items.Add(s3.ToString());
+
+            //check if s4 has an object
+            //if(s4 !=null)
+            //{
+            //    MessageBox.Show("It is point ing to an object");
+            //}
+            //else
+            //{
+            //    MessageBox.Show("s4 is null");
+            //}
         }
     }
 }
